77a4cb49a3eb6e92ff2973eb31f7fe7bd876fe24
FallenPhoenix8 <tplayxyz8@gmail.com>
2026-07-13 17:47:19 +0200

    Initial Commit

---
+++ Math Game/Math_GameApp.swift
@@ -0,0 +1,16 @@
+// 
+//  Math_GameApp.swift
+//  Math Game
+//
+    
+
+import SwiftUI
+
+@main
+struct Math_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
